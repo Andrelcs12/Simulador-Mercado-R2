471bab3589b626a34747a8e9c4440dea47ea708b
Gabriel-Carvalho09 <gabriel.sc990@gmail.com>
2026-03-15 19:40:45 -0300

    teste - gabrizzo -
    oi

--- simulador/api/src/app.service.ts
+++ simulador/api/src/app.service.ts
@@ -3,6 +3,13 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
+
     return 'Hello World!';
+
   }
+  async oi (){
+    return "oi";
+  }
+
+
 }
