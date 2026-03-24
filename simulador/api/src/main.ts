27bae7b3fdd9ba25154b1acd7fd0493505771b7b
André Lucas <apglu.26@gmail.com>
2026-03-24 20:11:18 -0300

    feat (arquivos do front)

--- simulador/api/src/main.ts
+++ simulador/api/src/main.ts
@@ -1,9 +1,30 @@
 import { NestFactory } from '@nestjs/core';
+import {
+  FastifyAdapter,
+  NestFastifyApplication,
+} from '@nestjs/platform-fastify';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+  const app = await NestFactory.create<NestFastifyApplication>(
+    AppModule,
+    new FastifyAdapter({ 
+      logger: true 
+    }),
+  );
+
+  // Configuração de CORS para permitir o Front-end
+  app.enableCors({
+    origin: ['http://localhost:3000'], 
+    credentials: true,
+    methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'],
+    allowedHeaders: ['Content-Type', 'Authorization'],
+  });
+
+  const port = process.env.PORT ?? 4000;
+  
+  await app.listen(port, '0.0.0.0');
+  console.log(`🚀 Novely API is running on: http://localhost:${port}`);
 }
 
-      bootstrap();
+bootstrap();
