e21ed5e057c224c5e0dc353ea470776b7b5ddc8f
André Lucas <apglu.26@gmail.com>
2026-03-24 15:13:30 -0300

    Update app.service.ts

--- simulador/api/src/app.service.ts
+++ simulador/api/src/app.service.ts
@@ -1,19 +1,16 @@
 import { Injectable } from '@nestjs/common';
+import { PrismaService } from './prisma.service';
 
 @Injectable()
 export class AppService {
+  
   getHello(): string {
-
     return 'Hello World!';
-
   }
+
   async oi (){
     return "oi teste1 do grba";
   }
 
-  async teste() {
-    return 'sla'
-  }
-  
 
 }
